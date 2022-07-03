826a51c4ec0ecec63915542fa3b4fd9a7c510517
adyluna <adylunaleite@gmail.com>
2022-07-03 12:21:33 -0300

    Requisito 3 ok

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,31 @@
-import React, { Component } from "react";
+import React, { Component } from 'react';
+import { getUser } from '../services/userAPI';
+import Carregando from '../pages/Carregando';
 
 class Header extends Component {
-    render() {
-      return (
-          
-        );
-    }
+  constructor() {
+    super();
+
+    this.state = {
+      isUserLoaded: null,
+    };
+  }
+
+  async componentDidMount() {
+    const user = await getUser();
+    this.setState({ isUserLoaded: user.name });
+  }
+
+  render() {
+    const { isUserLoaded } = this.state;
+    return (
+      <header data-testid="header-component">
+        { isUserLoaded
+          ? <h3 data-testid="header-user-name">{ isUserLoaded }</h3>
+          : <Carregando /> }
+      </header>
+    );
+  }
 }
 
 export default Header;
