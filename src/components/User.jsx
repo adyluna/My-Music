71dda3f2154d37ddcc63a4e3f83dd59a0187f108
adyluna <adylunaleite@gmail.com>
2022-07-09 21:01:10 -0300

    13 ok

---
+++ src/components/User.jsx
@@ -0,0 +1,34 @@
+import PropTypes from 'prop-types';
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+
+class User extends Component {
+  render() {
+    const { name, image, email, description } = this.props;
+    const editProfile = 'Editar perfil';
+    return (
+      <div>
+        <img
+          data-testid="profile-image"
+          className="profileImage"
+          src={ image }
+          alt={ image }
+        />
+        <Link to="/profile/edit">{editProfile}</Link>
+        <h3>{ name }</h3>
+        <h4>{ email }</h4>
+        <h5>Descrição</h5>
+        { description }
+      </div>
+    );
+  }
+}
+
+User.propTypes = {
+  description: PropTypes.string.isRequired,
+  email: PropTypes.string.isRequired,
+  image: PropTypes.string.isRequired,
+  name: PropTypes.string.isRequired,
+};
+
+export default User;
--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -1,11 +1,59 @@
 import React, { Component } from 'react';
 import Header from '../components/Header';
+import User from '../components/User';
+import { getUser } from '../services/userAPI';
+import Carregando from './Carregando';
 
 class Profile extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      user: false,
+    };
+  }
+
+  componentDidMount() {
+    this.handleLoadUser();
+  }
+
+  handleLoadUser = async () => {
+    const user = await getUser();
+    this.setState({ user });
+  };
+
+  RenderUser = () => {
+    const { user } = this.state;
+    // const description = `
+    //   Lorem ipsum dolor sit amet, consectetur adipiscing elit,
+    //   sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+    //   Ut enim ad minim veniam, quis nostrud exercitation ullamco
+    //   laboris nisi ut aliquip ex ea commodo consequat.
+    //   Duis aute irure dolor in reprehenderit in voluptate velit esse
+    //   cillum dolore eu fugiat nulla pariatur.
+    //   Excepteur sint occaecat cupidatat
+    //   non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.`;
+
+    // const defaultProfileImage = `https://media.istockphoto.com/vectors/
+    //   male-profile-flat-blue-simple-icon-with-long-shadow-vector-
+    //   id522855255?k=20&m=522855255&s=612x612&w=0&h=
+    //   fLLvwEbgOmSzk1_jQ0MgDATEVcVOh_kqEe0rqi7aM5A=`;
+    if (!user) {
+      return <Carregando />;
+    } return (<User
+      name={ user.name }
+      image={ user.image }
+      description={ user.description }
+      email={ user.email }
+    />);
+  }
+
   render() {
+    const { user } = this.state;
     return (
       <div data-testid="page-profile">
         <Header />
+        { user && <this.RenderUser /> }
       </div>
     );
   }
